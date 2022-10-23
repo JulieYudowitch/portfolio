7eefaab72741f4c9298a37b4b0b454b8c7e3e72d
julie yudowitch <julieyudo@gmail.com>
2022-10-23 05:46:19 -0400

    removed nav

--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,10 @@
 import Link from "next/link";
 import styles from '../styles/404.module.css';
-import Nav from "../components/Nav/Nav";
 import Footer from "../components/Footer/Footer";
 
 export default function FourOhFour(props) {
   return (
     <div className={styles.fourOhFourPage}>
-      <Nav />
       <div className={styles.fourOhFourPageContent}>
         <h1>404 | Not Found</h1>
         <Link href="/">
