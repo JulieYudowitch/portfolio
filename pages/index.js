378e2a45ba66493d0246b8e9292c45de988879b8
julie yudowitch <julieyudo@gmail.com>
2022-11-08 16:28:03 -0500

    removed test change

--- pages/index.js
+++ pages/index.js
@@ -66,7 +66,6 @@ export default function Home(props) {
             <p className={styles.name}>Front-End Developer | Next.js</p>
           </div>
           <div className={styles.homeIntroCard}>
-            <p className={styles.homeIntro}>test change</p>
             <p className={styles.homeIntro}>
               Hi I'm Julie Yudowitch and I build responsive webapps using
               Next.js and React. I studied Web Development on CodeCademy and
