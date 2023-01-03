5fc87284def0d5a55bacf77dabc0205db4c5c755
julie yudowitch <julieyudo@gmail.com>
2023-01-03 06:21:20 -0500

    styles

--- components/Banner/Banner.js
+++ components/Banner/Banner.js
@@ -3,39 +3,13 @@ import Image from "next/dist/client/image";
 
 function Banner() {
   return (
-    <header>
-      <div className={styles.banner}>
-        <div className={styles.bannerImg}>
-          <Image
-            src="/clouds.webp"
-            alt=""
-            priority
-            width="1072"
-            height="226"
-          />
-        </div>
-        <div className={styles.bannerImg2}>
-         
-        </div>
-        <div className={styles.bannerImg3}>
-          <Image
-            src="/pencils.webp"
-            alt=""
-            priority
-            width="1022"
-            height="526"
-          />
-        </div>
-        <div className={styles.homeTitleWrapper}>
-          <p className={styles.flexSpacer}>{""}</p>
+    <header>         
           <h1 className={styles.homeTitle}>
             JULIE YUDOWITCH<br></br>
             <span className={styles.homeTitleDescript}>
               WEB DEVELOPER | NEXT.JS
             </span>
           </h1>
-        </div>
-      </div>
     </header>
   );
 }
